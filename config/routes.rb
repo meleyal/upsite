a96a85fe196da0cd9fd02987b58dd35f0829d70c
meleyal <william.meleyal@gmail.com>
2013-01-27 14:19:36 +0100

    cleanup attachments controller and limit routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Sitespot::Application.routes.draw do
 
   constraints :subdomain => /.+/ do
     match '/' => 'sites#show'
-    resources :widgets
-    resources :texts
-    resources :images
-    resources :attachments
+    resources :widgets, except: [:new, :edit]
+    resources :texts, except: [:new, :edit]
+    resources :images, except: [:new, :edit]
+    resources :attachments, except: [:new, :edit]
   end
 
   root :to => 'sites#index'
