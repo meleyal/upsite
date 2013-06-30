0f578922ef66bf9ea32845967a882c7c4c539632
meleyal <william.meleyal@gmail.com>
2013-06-30 16:30:29 +0200

    - route all subdomains except www to app - only enable app routes in development for now...

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Pagekit::Application.routes.draw do
 
-  resources :signups, only: [:new, :create]
+  resources :signups, only: [:new]
 
-  # constraints :subdomain => /.+/ do
-  #   get '/' => 'pages#show'
-  #   put '/' => 'pages#update'
-  #   # resources :pages, only: [:show, :update], :path => '/'
-  #   resources :widgets, except: [:new, :edit]
-  #   resources :attachments, except: [:new, :edit]
-  #   # resources :attachments, only: [:create]
-  # end
+  if Rails.env.development?
+
+    constraints :subdomain => /^(?!www\Z)(\w+)/ do
+      get '/' => 'pages#show'
+      put '/' => 'pages#update'
+      # resources :pages, only: [:show, :update], :path => '/'
+      resources :widgets, except: [:new, :edit]
+      resources :attachments, except: [:new, :edit]
+      # resources :attachments, only: [:create]
+    end
+
+  end
 
   root :to => 'signups#new'
 
