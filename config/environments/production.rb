d73f08f975a3424e28121425d5d2561cb6a61097
William Meleyal <william.meleyal@gmail.com>
2016-03-12 13:13:17 +0000

    sendgrid config...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   }
 
   # Email config.
-  config.action_mailer.default_url_options = { host: 'upsite.io' }
+  config.action_mailer.default_url_options = { host: 'upsite.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -101,7 +101,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'upsite.herokuapp.com',
     :enable_starttls_auto => true
   }
 end
