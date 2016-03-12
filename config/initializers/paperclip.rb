86595ac9e883a21dda92f0cfaae5ac4e1cd59704
William Meleyal <william.meleyal@gmail.com>
2016-03-12 11:42:26 +0000

    schemaless paperclip urls

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,3 +3,4 @@ Paperclip.interpolates :app_name do |attachment, style|
 end
 
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_protocol] = ''
