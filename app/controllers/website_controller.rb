6cce85444a06f3446dee0b5e2ee6b0624f48abb4
William Meleyal <william.meleyal@gmail.com>
2016-04-11 13:39:01 +0100

    add ability to hide sites from home page with featured = false

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -2,6 +2,6 @@ class WebsiteController < ApplicationController
   skip_before_action :authenticate, only: [:index, :terms, :privacy]
 
   def index
-    @sites = Site.active.order(:featured, updated_at: :desc).limit(100)
+    @sites = Site.active.where("featured IS NOT false").order(:featured, updated_at: :desc).limit(100)
   end
 end
