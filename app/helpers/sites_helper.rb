a08a1bee27a47fc0417bf2a7b8137f1f8ee03690
William Meleyal <william.meleyal@gmail.com>
2016-04-15 20:15:26 +0100

    Fix: open graph preview images

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -2,18 +2,15 @@ module SitesHelper
   def site_favicon_link(site)
     if site.color
       color = site.color.gsub('#', '')
-      "//placeholdit.imgix.net/~text?bg=#{color}&w=32&h=32".html_safe
+      "http://placeholdit.imgix.net/~text?bg=#{color}&w=32&h=32".html_safe
     else
       '/favicon.ico?v=4'
     end
   end
 
-  def site_preview_image(site)
-    if site.images.first
-      site.images.first.image_2x
-    else
-      color = site.color.present? ? site.color.gsub('#', '') : '00ba6b'
-      "//placeholdit.imgix.net/~text?bg=#{color}&w=1200&h=630".html_safe
-    end
+  def site_preview_image_link(site)
+    # site.images.first.try(:image_2x)
+    color = site.color.present? ? site.color.gsub('#', '') : 'eeeeee'
+    "http://placeholdit.imgix.net/~text?bg=#{color}&w=600&h=600".html_safe
   end
 end
