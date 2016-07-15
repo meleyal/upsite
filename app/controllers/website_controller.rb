0967c362e380799b6067d18c722f83475635953c
William Meleyal <william.meleyal@gmail.com>
2016-07-15 15:56:31 +0100

    improve filtering of homepage

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -2,6 +2,18 @@ class WebsiteController < ApplicationController
   skip_before_action :authenticate, only: [:index, :legal]
 
   def index
-    @sites = Site.active.where("featured IS NOT false").order(:featured, updated_at: :desc).limit(100)
+    if params[:filter] == 'all'
+      @sites = Site.active.order(updated_at: :desc)
+    else
+      @sites = Site.active
+        .joins(:blocks)
+        .includes(:attachments)
+        .group('sites.id')
+        .having('count(site_id) > 3')
+        .where('featured IS NOT false')
+        .where.not(color: '')
+        .order(updated_at: :desc)
+        .limit(100)
+    end
   end
 end
