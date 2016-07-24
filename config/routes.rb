8c898f9444c9a3b06c7adb041f7476f0a7165209
William Meleyal <william.meleyal@gmail.com>
2016-07-24 03:10:00 +0100

    remove blog link

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,6 @@ Rails.application.routes.draw do
     get '/legal', to: 'website#legal'
     get '/business', to: 'landing_pages#business'
     get '/creative', to: 'landing_pages#creative'
-    get '/blog', to: redirect('http://upsiteapp.tumblr.com/')
     get '/donate', to: redirect('https://www.patreon.com/upsiteapp')
     root 'website#index'
   end
