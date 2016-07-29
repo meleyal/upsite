a7fcc9ffac3c053d8c676595f384901f444e1b79
William Meleyal <william.meleyal@gmail.com>
2016-07-29 10:40:53 +0100

    revert last commit

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -2,6 +2,6 @@ class NotificationsMailer < ApplicationMailer
   def help_email(user, message)
     @user = user
     @message = message
-    mail(to: 'help@upsite.io', from: @user.email, subject: 'Upsite / Help')
+    mail(to: 'help@upsite.io', reply_to: @user.email, subject: 'Upsite / Help')
   end
 end
