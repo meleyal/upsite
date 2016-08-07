f88fe912577e44c37b3ad84be3d85a9aefca2cc2
William Meleyal <william.meleyal@gmail.com>
2016-08-07 03:18:00 +0200

    remove signup email

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,10 +1,4 @@
 class NotificationsMailer < ApplicationMailer
-  def signup_email(user, url)
-    @user = user
-    @url = url
-    mail(to: 'notifications@upsite.io', reply_to: @user.email, subject: 'Welcome to Upsite')
-  end
-
   def help_email(user, message)
     @user = user
     @message = message
