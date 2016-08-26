8a17e8ae66a25c11ed08d278d44f51cd953932c9
William Meleyal <william.meleyal@gmail.com>
2016-08-26 20:52:00 +0100

    landing page setup

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -2,6 +2,14 @@ class LandingPagesController < ApplicationController
   skip_before_action :authenticate
   before_action :set_site
   layout 'website'
+  PAGES = %w(simple)
+
+  def index
+    render(status: 404) unless PAGES.include?(params[:slug])
+    # version = params[:version] || 'a'
+    # render "index_#{version}"
+    render 'index_a'
+  end
 
   private
 
