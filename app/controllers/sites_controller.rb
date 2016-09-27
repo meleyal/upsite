a44d5933027bbac80c385516fc683a7dfb18ab10
William Meleyal <william.meleyal@gmail.com>
2016-09-27 22:27:32 +0100

    remove pro stuff, change to add-ons

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -27,15 +27,12 @@ class SitesController < ApplicationController
   end
 
   def upgrade
-    if cookies[:clicked_upgrade_button_experiment].present?
-      render 'upgrade_confirm'
-    else
-      render 'upgrade'
-    end
+    NotificationsMailer.analytics_email(:upgrade, current_user, root_url(subdomain: @site.subdomain), :custom_url).deliver_now
+    render 'upgrade'
   end
 
   def upgrade_confirm
-    cookies.permanent[:clicked_upgrade_button_experiment] = true
+    cookies.permanent[:clicked_custom_url_addon_experiment] = true
     NotificationsMailer.analytics_email(:upgrade, current_user, root_url(subdomain: @site.subdomain), params[:source]).deliver_now
     flash[:analytics_upgrade] = true
   end
