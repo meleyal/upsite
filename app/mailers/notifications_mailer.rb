02309041887f4c8f32b7f2b0db5c2444b28a327b
William Meleyal <william.meleyal@gmail.com>
2016-11-06 10:44:34 +0000

    Remove emojis from notification emails for now

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,11 +1,11 @@
 class NotificationsMailer < ApplicationMailer
   def welcome_email(user, site_url)
     @site_url = site_url
-    mail(to: user.email, subject: 'Welcome to Upsite U+1F44B')
+    mail(to: user.email, subject: 'Welcome to Upsite')
   end
 
   def upgrade_email(user)
-    mail(to: user.email, subject: 'Thanks for upgrading to Upsite Pro U+1F389')
+    mail(to: user.email, subject: 'Thanks for upgrading to Upsite Pro')
   end
 
   def help_email(user, message)
