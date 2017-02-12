d06ff094375a0f760d236ae138f3aa61b589fae5
William Meleyal <william.meleyal@gmail.com>
2017-02-12 13:50:20 -0300

    Introduce Pro 10 plan

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -3,7 +3,7 @@ class Plan < ActiveRecord::Base
   has_many :users, through: :subscriptions
 
   def self.pro
-    self.find_by(code: 'pro_1')
+    self.find_by(code: 'pro_10')
   end
 
   def self.promo
