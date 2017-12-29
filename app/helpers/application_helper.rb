b3a77a92a973eba27ae195e1673587e3138ac351
William Meleyal <william.meleyal@gmail.com>
2017-12-29 17:52:58 +0000

    Fix markdown new lines

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def markdown(text)
-    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(with_toc_data: true), hard_wrap: true, autolink: true)
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(with_toc_data: true, hard_wrap: true, autolink: true))
     @markdown.render(text).html_safe
   end
 
